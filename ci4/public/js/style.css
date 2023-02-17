b332460670ca45591f3acf93c394b639aa10fd80
KimAltea <kealtea@student.apc.edu.ph>
2023-02-17 09:08:32 +0800

    update

---
+++ ci4/public/js/style.css
@@ -0,0 +1,61 @@
+@import url('https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap');
+
+*{
+    box-sizing: border-box;
+    list-style: none;
+    font-family: 'Josefin Sans', sans-serif;
+}
+
+body{
+    background-image: url(https://images.unsplash.com/photo-1432847712612-926caafaa802?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80);
+    margin: 10px;
+    padding: 10px;
+}
+
+.wrapper{
+    position: absolute;
+    top: 50%;
+    left: 45%;
+    transform: translate(-50%,-50%);
+    width: 450px;
+    display: flex;
+    box-shadow: : 0 1px, 20px 0 rgba(60,90,100,0.08)
+}
+
+.wrapper .left{
+    width: 450%;
+    padding: 30px 25px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    text-align: center;
+}
+.wrapper .left img{
+    margin-bottom: 15px;
+    border-radius: 5px;
+}
+
+.wrapper .left h4{
+    margin-bottom: 10px;
+}
+.wrapper .left p{
+    font-size: 12px;
+}
+
+.wrapper .right{
+    width: 500%;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+    padding: 30px 25px;
+}
+.wrapper .right .info,
+.wrapper .right .project{
+    margin-bottom: 25px;
+}
+.wrapper .right .info h3,
+.wrapper .right .project h3{
+    margin-bottom: 15px;
+    padding-bottom: 5px;
+    border-bottom: 1px solid #e0e0e0;
+    text-transform: uppercase;
+    letter-spacing: 5px;
+}
